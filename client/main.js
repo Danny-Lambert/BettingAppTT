c907bd9185adf0418e0383df45c42ce8d8fe9b25
Danny Lambert <dlambert150293@gmail.com>
2021-01-28 12:40:04 +0000

    Final working version for submission

--- client/main.js
+++ client/main.js
@@ -1,25 +1,16 @@
-// import { response } from "express";
 
-// import { response } from "express";
-
-const sendToMongo =  (result) => {
+const sendToMongo =  (result, endpointExt) => {
     console.log("I'm going to submit");
-    // 3. TODO!! Let's make POST request to our API...
-    // *********************************
     const fetchOptions = {
-      // This is a POST Request
         method: 'POST',
-      // We are gonna send JSON
         headers: { 'Content-Type': 'application/json' },
-      // Data goes in the body
-      body: JSON.stringify({"name": result}) // this is the data which we are sending inside the post request 
+        body: JSON.stringify({"name": result}) 
     }
-    fetch('http://localhost:8080/create', fetchOptions)// this is referring to line 35 where we dont want to fetch, we want to post 
+    fetch(`http://localhost:8080/create${endpointExt}`, fetchOptions)
         .then(res => res.json())
         .then(res => {
-            console.log("YAY WE GOT OUR RESPONSE BACK....")
-        // handleFetch(); /// dont think we need this as we dont need to bring anything back 
-      })
+            console.log("WE GOT OUR RESPONSE BACK....")
+    })
 }
 const sportData = () => {
 fetch("https://api.the-odds-api.com/v3/sports/?apiKey=a8c3c43277cb3421d02ab70bba310be8")
@@ -32,15 +23,11 @@ fetch("https://api.the-odds-api.com/v3/sports/?apiKey=a8c3c43277cb3421d02ab70bba
     .then ((jsonResponse) => {
         const sportsData = jsonResponse.data.map(sport => {
             console.log(sport.group)
-            sendToMongo(sport.group)
+            sendToMongo(sport.group, "Sports")
             return `<li> ${sport.group} - ${sport.details} </li>`
         })
         document.querySelector(".sportsAvailable").innerHTML = sportsData;
-        // object.addEventListener("load", sendToMongo);
     })
-    // .then ((jsonResponse) => {
-    //     jsonResponse.data.map(sport => console.log(sport))
-    // })
     .catch((error) => {
         throw Error ("error")
     })
@@ -58,23 +45,20 @@ const inPlay = () => {
             return response.json();
         })
             .then ((jsonResponse) => {
-                // console.log(jsonResponse.data)
                 const result = []
                 const inPlayFixtures = jsonResponse.data.map(upcoming => {
                     console.log(upcoming.teams)
+                    sendToMongo(upcoming.teams, "Fixtures")
                     return `
                     <div class = "inPlayGames"> 
                     <p class = "fixtures"> ${upcoming.sport_nice} fixture - ${upcoming.teams[0]} vs ${upcoming.teams[1]}  </p>
                     <p class = "upcomingOdds"> ${upcoming.sites[0].site_nice} Odds - Home win (${upcoming.sites[0].odds.h2h[0]}) Draw (${upcoming.sites[0].odds.h2h[2]}) Away win (${upcoming.sites[0].odds.h2h[1]}) <p/>
                     <p class = "upcomingOdds"> ${upcoming.sites[1].site_nice} Odds - Home win (${upcoming.sites[1].odds.h2h[0]}) Draw (${upcoming.sites[0].odds.h2h[2]}) Away win (${upcoming.sites[0].odds.h2h[1]}) <p/>
                     </div>
-                    `
-                    // .then ((inPlayFixtures.sendToMongo))
-                    
+                    `                 
                 
                 })
                         document.querySelector(".upcomingFixtures").innerHTML = inPlayFixtures;
-                        // console.log (result)
                 })
                 .catch((error) => {
                     throw Error ("error")
@@ -95,7 +79,6 @@ const inPlay = () => {
             return response.json();
         })
         .then ((jsonResponse) => {
-                // console.log(jsonResponse.data)
     
         const premLeagueFixt = jsonResponse.data.map(premierLeague => {
             return ` 
--- server/app.js
+++ server/app.js
@@ -9,20 +9,21 @@ const app = express()
 app.use(cors())
 app.use(bodyParser.json())
 
-//from dannysnodeapi file
-let users = [];
-// GET endpoint which returns/sends all our users in the response
+
 app.get('/', async (request, response) => {
-    const users = await getCollectionDocuments(' inPlay')
-    response.send(users);
+    const returnedData = await getCollectionDocuments('Sports')
+    response.send(returnedData);
 })
-// POST endpoint which takes the user from the request body and saves it...
-app.post('/create', async (request, response) => {
-    const newUser = request.body;
-    await createCollectionDocument(' inPlay', newUser);
-    response.send({message: "We created this user...."})
+app.post('/createsports', async (request, response) => {
+    const newEntry = request.body;
+    await createCollectionDocument('Sports', newEntry);
+    response.send({message: "We created a new entry...."})
 })
-// Finally! Listen on Port 8080
+app.post('/createfixtures', async (request, response) => {
+    const newEntry = request.body;
+    await createCollectionDocument('Fixtures', newEntry);
+    response.send({message: "We created a new entry...."})
+})
 app.listen(8080, () => {
     console.log("port running")
 })
